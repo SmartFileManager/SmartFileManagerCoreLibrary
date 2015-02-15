e3aacfb9fa9afc2d30359bee1fd94957c2d6dc91
dev23cc <dev23cc@yandex.com>
2015-02-16 01:46:36 +0200

    First commit

---
+++ src/main/java/gr/teicm/pm/smartfilemanager/smartfilemanagercorelibrary/entity/logic/EntityChangedListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.teicm.pm.smartfilemanager.smartfilemanagercorelibrary.entity.logic;
+
+/**
+ *
+ * @author ToNio
+ */
+public interface EntityChangedListener {
+
+    public void entityChanged(EntityChangedEvent event);
+
+}
---
+++ src/main/java/gr/teicm/pm/smartfilemanager/smartfilemanagercorelibrary/entity/logic/file/FileSystemRootNode.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.teicm.pm.smartfilemanager.smartfilemanagercorelibrary.entity.logic.file;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ToNio
+ */
+public class FileSystemRootNode extends AbstractFile {
+
+    private List<AbstractFile> children;
+
+    protected FileSystemRootNode() throws IOException {
+        super("", null);
+        super.setAlias("ParentOfRootDirs");
+        this.children = new ArrayList<>();
+        intializeChildren();
+    }
+
+    @Override
+    protected boolean isFolderish() {
+        return true;
+    }
+
+    private void intializeChildren() throws IOException {
+        Iterable<Path> fileSystems = FileSystems.getDefault().getRootDirectories();
+        for (Path fileSystem : fileSystems) {
+            children.add(new RootChild(fileSystem, this));
+        }
+    }
+
+    @Override
+    protected List<AbstractFile> getAllChildren() {
+        return children;
+    }
+
+    @Override
+    protected String getCharNameOnly() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String getFullName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
